db6eaf4504c3d054098e73b74fad5af24d979e0f
Varad Deshmukh <99480015+VaradDeshmukh18@users.noreply.github.com>
2023-10-08 16:42:03 +0530

    Create profile_updated.js

---
+++ profile_updated.js
@@ -0,0 +1,79 @@
+let Profiles = [
+ {
+   name: "Vaddy",
+   imageLink: "https://staticg.sportskeeda.com/editor/2022/06/508cd-16557590423635-1920.jpg",
+   twitterLink: "",
+   facebookLink: "",
+   githubLink: "",
+   linkedinLink: "https://www.linkedin.com/in/vaddy69/",
+   instagramLink: "",
+ },
+  
+  {
+    name: "Tipchan Songtheng",
+    imageLink:
+      "https://pbs.twimg.com/profile_images/1710009010873872384/Sohs7yX2_400x400.jpg",
+    twitterLink: "https://twitter.com/tipchan_s",
+    facebookLink: "",
+    githubLink: "https://github.com/tsongtheng",
+    linkedinLink: "https://www.linkedin.com/in/tipchan-songtheng-5417991a8/",
+    instagramLink: "",
+  },
+  {
+    name: "Chammoun Chakhap",
+    imageLink: "https://avatars.githubusercontent.com/u/43007452?v=4",
+    twitterLink: "",
+    facebookLink: "",
+    githubLink: "https://github.com/chammounc",
+    linkedinLink: "https://www.linkedin.com/in/chammoun-chakhap-771093171",
+    instagramLink: "",
+  },
+  {
+    name: "Esin Ridollah",
+    imageLink: "https://avatars.githubusercontent.com/u/86829637?v=4",
+    twitterLink: "https://twitter.com/esinnation",
+    facebookLink: "",
+    githubLink: "https://github.com/esinnation",
+    linkedinLink: "https://www.linkedin.com/in/esin-ridollah/",
+    instagramLink: "https://www.instagram.com/esinnation/",
+  },
+  {
+    name: "ISHAN AHMAD",
+    imageLink: "",
+    twitterLink: "",
+    facebookLink: "",
+    githubLink: "https://github.com/ishan-nahid",
+    linkedinLink: "",
+    instagramLink: "",
+  },
+  {
+    name: "aecuto",
+    imageLink: "",
+    twitterLink: "",
+    facebookLink: "",
+    githubLink: "https://github.com/aecuto",
+    linkedinLink: "",
+    instagramLink: "",
+  },
+  {
+   name: "Mayank-Creater",
+   imageLink: "https://avatars.githubusercontent.com/u/67818466?v=4",
+   twitterLink: "",
+   facebookLink: "",
+   githubLink: "https://github.com/Mayank-Creater",
+   linkedinLink: "",
+   instagramLink: "",
+ },
+
+ {
+  name:"Jagpreet Singh Khurana",
+  imageLink:"",
+  twitterLink: "",
+   facebookLink: "",
+   githubLink: "https://github.com/Jagpreet153",
+   linkedinLink: "",
+   instagramLink: "",
+ }
+];
+
+export default Profiles;
